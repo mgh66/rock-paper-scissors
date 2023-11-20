688655dbdcaefb15a51a98ee41acb46a42028760
mgh66 <mohammad.maghsoodi66@gmail.com>
2023-11-20 21:15:35 +0330

    Add player selection

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function getComputerChoice() {
-    let computerChoice = function (){
+    function computerChoice() {
     return Math.floor(Math.random()*3);
     }
     function getComputerSelection(){
@@ -18,6 +18,24 @@ function getComputerChoice() {
     return getComputerSelection();
 }
 const computerSelection = getComputerChoice();
+// The section related to player selection
+function getPlayerChoice() {
+    function playerChoice() {
+        let userInput = prompt('What is your choice between Rock, Paper and Scissors?');
+        return userInput.charAt(0).toUpperCase() + userInput.slice(1).toLowerCase();
+    }
+    let getPlayerSelection = playerChoice();
+    if (getPlayerSelection==='Rock'
+    ||getPlayerSelection==='Paper'
+    ||getPlayerSelection==='Scissors') {
+        return getPlayerSelection;
+    }
+alert `Please choose between rock, paper and scissors.`
+}
+const PlayerSelection = getPlayerChoice();
+
+
+
 
 
 
