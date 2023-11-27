8858af009323a528b975433840906c5a2803bbde
mgh66 <mohammad.maghsoodi66@gmail.com>
2023-11-27 19:57:31 +0330

    Add Add the whole game

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ function getComputerChoice() {
     switch (computerChoice()) {
         case 0:
             return 'Rock';
-            break;
         case 1:
             return 'Paper';
-            break;
         case 2:
             return 'Scissors'        
     }
@@ -19,6 +17,9 @@ function getComputerChoice() {
 function getPlayerChoice() {
     function playerChoice() {
         let userInput = prompt('What is your choice between Rock, Paper and Scissors?');
+        if (userInput===null||userInput==='') {
+            return playerChoice();
+        }
         return userInput.charAt(0).toUpperCase() + userInput.slice(1).toLowerCase();
     }
     let getPlayerSelection = playerChoice();
@@ -26,15 +27,17 @@ function getPlayerChoice() {
     ||getPlayerSelection==='Paper'
     ||getPlayerSelection==='Scissors') {
         return getPlayerSelection;
+    } else {
+        console.log ('Please choose between rock, paper and scissors.')
+        return getPlayerSelection;
     }
-return `Please choose between rock, paper and scissors.`
 }
 let playerSelection;
 let computerSelection;
 //The section related to the execution of each round of the game
 let playerScore = 0;
 let computerScore = 0;
-function playRound(playerSelection, computerSelection) {
+function playRound() {
     playerSelection = getPlayerChoice()
     computerSelection = getComputerChoice();
     if (playerSelection==='Rock'&&computerSelection==='Scissors') {
@@ -65,8 +68,49 @@ function playRound(playerSelection, computerSelection) {
         console.log(`This round was tied. You choose ${playerSelection} and Computer choose ${computerSelection}.
         You're score: ${playerScore}
         computer score: ${computerScore}`);
-        return playRound(playerSelection, computerSelection);
+        return playRound();
     } else {
         console.log(`The game does not run.`);
+        return playRound();
     }
 }
+//Determining the report of each round along with determining the final winner and ending the game
+function getGameReport() {
+    if (playerScore===3) {
+        console.log ('You Win');
+        return true;
+    } else if (computerScore===3) {
+        console.log ('You Lose');
+        return true;
+    } else if (playerScore===1 && computerScore===0) {
+        console.log ('Great! You are ;ahead!')
+    } else if (playerScore===1 && computerScore===1) {
+        console.log ('keep trying.');
+    } else if (playerScore===2 && computerScore===0) {
+        console.log ('You are doing great!');
+    } else if (playerScore===2 && computerScore===1) {
+        console.log ('You are very close to winning the game!');
+    } else if (playerScore===2 && computerScore===2) {
+        console.log ('This is the last round. Choose carefully.');
+    } else if (playerScore===0 && computerScore===1) {
+        console.log ('You just lost the first round! It is not over yet.');
+    }  else if (playerScore===0 && computerScore===2) {
+        console.log ('You are close to losing the game.');
+    } else {
+        console.log ('You must make the right choice to win!');
+    } 
+}
+function game() {
+playRound();
+if (getGameReport()) return;
+playRound();
+if (getGameReport()) return;
+playRound();
+if (getGameReport()) return;
+playRound();
+if (getGameReport()) return;
+playRound();
+if (getGameReport()) return;
+}
+game();
+
