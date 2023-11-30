0130bac84708ec86fbab765e723e0eabb7d8f092
mgh66 <mohammad.maghsoodi66@gmail.com>
2023-11-30 17:24:16 +0330

    Add game report and score report in game function

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ function getComputerChoice() {
             return 'Scissors'        
     }
 }
-
 // The section related to player selection
 function getPlayerChoice() {
     function playerChoice() {
@@ -37,79 +36,81 @@ let computerSelection;
 //The section related to the execution of each round of the game
 let playerScore = 0;
 let computerScore = 0;
-function playRound() {
+function playRound(playerSelection, computerSelection) {
     playerSelection = getPlayerChoice()
     computerSelection = getComputerChoice();
     if (playerSelection==='Rock' && computerSelection==='Scissors') {
-        console.log(`You Win! Rock beats Scissors.
-        You're score: ${++playerScore}
-        computer score: ${computerScore}`);
+        playerScore++;
+        return `You Win! Rock beats Scissors.`
     } else if (playerSelection==='Scissors'&& computerSelection==='Paper') {
-        console.log(`You Win! Scissors beats Paper.
-        You're score: ${++playerScore}
-        computer score: ${computerScore}`);
+        playerScore++;
+        return `You Win! Scissors beats Paper.`
     } else if (playerSelection==='Paper' && computerSelection==='Rock') {
-        console.log(`You Win! Paper beats Rock.
-        You're score: ${++playerScore}
-        computer score: ${computerScore}`);
+        playerScore++;
+        return `You Win! Paper beats Rock.`
     } else if (computerSelection==='Rock' && playerSelection==='Scissors') {
-        console.log(`You Lose! Rock beats Scissors.
-        You're score: ${playerScore}
-        computer score: ${++computerScore}`);
+        computerScore++;
+        return `You Lose! Rock beats Scissors.`
     } else if (computerSelection==='Scissors' && playerSelection==='Paper') {
-        console.log(`You Lose! Scissors beats Paper.
-        You're score: ${playerScore}
-        computer score: ${++computerScore}`);
+        computerScore++;
+        return `You Lose! Scissors beats Paper.`
     } else if (computerSelection==='Paper' && playerSelection==='Rock') {
-        console.log(`You Lose! Paper beats Rock.
-        You're score: ${playerScore}
-        computer score: ${++computerScore}`);
+        computerScore++;
+        return `You Lose! Paper beats Rock.`
     } else if (playerSelection===computerSelection) {
-        console.log(`This round was tied. You choose ${playerSelection} and Computer choose ${computerSelection}.
-        You're score: ${playerScore}
-        computer score: ${computerScore}`);
-        return playRound();
+        console.log(`This round was tied. You choose ${playerSelection} and Computer choose ${computerSelection}.`);
+        return playRound(playerSelection, computerSelection);
     } else {
         console.log(`The game does not run.`);
-        return playRound();
+        return playRound(playerSelection, computerSelection);
     }
 }
 //Determining the report of each round along with determining the final winner and ending the game
-function getGameReport() {
-    if (playerScore===3) {
-        console.log('You Win');
-        return true;
-    } else if (computerScore===3) {
-        console.log('You Lose');
-        return true;
-    } else if (playerScore===1 && computerScore===0) {
-        console.log('Great! You are ;ahead!')
-    } else if (playerScore===1 && computerScore===1) {
-        console.log('keep trying.');
-    } else if (playerScore===2 && computerScore===0) {
-        console.log('You are doing great!');
-    } else if (playerScore===2 && computerScore===1) {
-        console.log('You are very close to winning the game!');
-    } else if (playerScore===2 && computerScore===2) {
-        console.log('This is the last round. Choose carefully.');
-    } else if (playerScore===0 && computerScore===1) {
-        console.log('You just lost the first round! It is not over yet.');
-    }  else if (playerScore===0 && computerScore===2) {
-        console.log('You are close to losing the game.');
-    } else {
-        console.log('You must make the right choice to win!');
-    } 
-}
 function game() {
-playRound();
+    function getGameReport() {
+        if (playerScore===3) {
+            console.log ('You Win');
+            return true;
+        } else if (computerScore===3) {
+            console.log('You Lose');
+            return true;
+        } else if (playerScore===1 && computerScore===0) {
+            console.log('Great! You are ;ahead!')
+        } else if (playerScore===1 && computerScore===1) {
+            console.log('keep trying.');
+        } else if (playerScore===2 && computerScore===0) {
+            console.log('You are doing great!');
+        } else if (playerScore===2 && computerScore===1) {
+            console.log('You are very close to winning the game!');
+        } else if (playerScore===2 && computerScore===2) {
+            console.log('This is the last round. Choose carefully.');
+        } else if (playerScore===0 && computerScore===1) {
+            console.log('You just lost the first round! It is not over yet.');
+        }  else if (playerScore===0 && computerScore===2) {
+            console.log('You are close to losing the game.');
+        } else {
+            console.log ('You must make the right choice to win!');
+        } 
+    }
+    function showScores() {
+        return `
+        You're score: ${playerScore}
+        Computer score: ${computerScore}`
+    }
+console.log(playRound(playerSelection, computerSelection));
+console.log(showScores());
 if (getGameReport()) return;
-playRound();
+console.log(playRound(playerSelection, computerSelection));
+console.log(showScores());
 if (getGameReport()) return;
-playRound();
+console.log(playRound(playerSelection, computerSelection));
+console.log(showScores());
 if (getGameReport()) return;
-playRound();
+console.log(playRound(playerSelection, computerSelection));
+console.log(showScores());
 if (getGameReport()) return;
-playRound();
+console.log(playRound(playerSelection, computerSelection));
+console.log(showScores());
 if (getGameReport()) return;
 }
 game();
